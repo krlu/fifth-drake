02e3c7e3c2558ab586e62d7f65e34b88c67b3e26
Michael Wang <ziyuanwang@utexas.edu>
2016-08-24 19:38:03 -0500

    initial teamfight, skirmish, roam, dragon

--- src/main/scala/gg/climb/Events.scala
+++ src/main/scala/gg/climb/Events.scala
@@ -1,10 +1,9 @@
 package gg.climb
 
-import gg.climb.analytics.EventStreamFactory
-import gg.climb.lolobjects.esports.{Bot, Player, Role, Support}
+import gg.climb.lolobjects.esports.Player
 import gg.climb.lolobjects.game.LocationData
-import gg.climb.lolobjects.game.state.{GameState, PlayerState, SideColor}
-import gg.climb.ramenx.core.{EventStream, ListEventStream}
+import gg.climb.lolobjects.game.state.PlayerState
+import gg.climb.ramenx.core.{Behavior, EventStream}
 
 import scala.concurrent.duration._
 
@@ -12,15 +11,24 @@ import scala.concurrent.duration._
   * Created by michael on 8/4/16.
   */
 object Events {
-  def main(args: Array[String]): Unit = {
-    val swap = laneswap(1001790061).scan(false, (_,_) => true)
-  }
+
+  /**
+    * Represents a group of players involved in an event
+    */
+  type Group = Set[Player]
 
   /**
     * A gank has occurred if:
     * 1) player exits roaming
-    * 2) skirmish occurs within 15 seconds
+    * 2) skirmish occurs within 10 seconds
+    * 3) less than 3 turrets taken by either team
     */
+  def gank(players: Behavior[Duration, Map[Player, PlayerState]],
+           turrets: EventStream[Duration, (Int, Int)]): EventStream[Duration, Map[Group, Group]] = {
+    val roams = roam(players)
+    val skirmishes = skirmish(players)
+    ???
+  }
 
   /**
     * A teamfight has occurred if:
@@ -29,8 +37,8 @@ object Events {
     * 3) hp loss above 100 per second
     * 4) mp usage above 100 per second
     */
-  def teamfight(game: Int): EventStream[Duration, Set[Set[PlayerState]]] = {
-    fight(game, 100, 100, 2000, 8, 10)
+  def teamfight(players: Behavior[Duration, Map[Player, PlayerState]]): EventStream[Duration, Set[Group]] = {
+    fight(players, 100, 100, 2000, 8, 10)
   }
 
   /**
@@ -40,61 +48,70 @@ object Events {
     * 3) hp loss above 100 per second
     * 4) mp usage above 100 per second
     */
-  def skirmish(game: Int): EventStream[Duration, Set[Set[PlayerState]]] = {
-    fight(game, 100, 100, 1500, 2, 7)
+  def skirmish(players: Behavior[Duration, Map[Player, PlayerState]]): EventStream[Duration, Set[Group]] = {
+    fight(players, 100, 100, 1500, 2, 7)
   }
 
-  private def fight(game: Int, hpLossPerSec: Double, mpLossPerSec: Double,
-                    maxDist: Double, minPlayers: Int, maxPlayers: Int): EventStream[Duration, Set[Set[PlayerState]]] = {
+  private def fight(players: Behavior[Duration, Map[Player, PlayerState]],
+                    hpLossThreshold: Double,
+                    mpLossThreshold: Double,
+                    maxDist: Double,
+                    minPlayers: Int,
+                    maxPlayers: Int): EventStream[Duration, Set[Group]] = {
 
-    def bothSidesInFight(set: Set[PlayerState]): Boolean = {
-      set.exists(ps => ps.sideColor.equals(SideColor.RED)) && set.exists(ps => ps.sideColor.equals(SideColor.BLUE))
-    }
+    def hpLoss: Behavior[Duration, Map[Player, Double]] = players.withPrev(Duration(5, SECONDS))
+      .map({
+        case (Some(x), y) => y.map({
+          case (player, state) => player -> (x(player).championState.hp - state.championState.hp)
+        })
+      })
 
-    val data = EventStreamFactory.gameStateStream(game)
-    val windows = makeWindows(data, Duration(5, SECONDS))
-      .map(w => {
-        val es: EventStream[Duration, List[PlayerState]] = w.map(gs => gs.players)
-        // All fights for a 5 second time window
-        var fights = Set.empty[Set[PlayerState]]
-        for (i <- 0 until es.first._2.size - 1) {
-          var possibleFight = Set.empty[PlayerState]
-          val psI = es.map(ps => ps(i))
+    def mpLoss = players.withPrev(Duration(5, SECONDS))
+      .map({
+        case (Some(x), y) => y.map({
+          case (player, state) => player -> (x(player).championState.mp - state.championState.mp)
+        })
+      })
 
-          // Only consider p(i) if meets hp/mp conditions
-          if (hpLossMet(psI, hpLossPerSec) && mpLossMet(psI, mpLossPerSec)) {
-            possibleFight += psI.first._2
-            // Set of other players that match distance
-            val psISet = psI.map(ps => Set(ps))
-            for (j <- i + 1 until es.first._2.size) {
-              val psJ = es.map(ps => ps(j))
-              // Compare p(i) with each p(j) for distance
-              psISet.merge(psJ.map(ps => Set(ps)), (p1, p2) => p1.union(p2))
-                .filter(set => distanceMet(set.head.location, set.last.location, maxDist))
-                // union each
-                .getAll.foreach(e => possibleFight.union(e._2))
-            }
+    def partition(states: Map[Player, PlayerState]): Set[Group] = {
 
-            // Add all possible fights to current time window
-            if (possibleFight.size > 1 && bothSidesInFight(possibleFight)) {
-              var found = false
-              fights.foreach(fight => {
-                if (fight.intersect(possibleFight).nonEmpty) {
-                  fights += fight ++ possibleFight
-                  fights -= fight
-                  found = true
-                }
-              })
+      def inFightRange(a: LocationData, b: LocationData, max: Double): Boolean = {
+        math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) <= max
+      }
 
-              if (!found) {
-                fights += possibleFight
+      var groups = states.mapValues(ps => Set(ps))
+
+      groups.foreach({
+        case (p1, g1) => groups.foreach({
+          case (p2, g2) =>
+            if (!p1.equals(p2)) {
+              if (g1.exists(ps1 => g2.exists(ps2 => inFightRange(ps1.location, ps2.location, maxDist)))) {
+                groups += p1 -> (g1 union g2)
+                groups -= p2
               }
             }
-          }
-        }
-
-        fights.filter(set => set.size >= minPlayers && set.size <= maxPlayers)
+        })
       })
+
+      groups.toSet.map({case (player, group) => group.map(ps => ps.player)})
+    }
+
+    def groups: Behavior[Duration, Set[Group]] = players.map(partition)
+
+    def filterGroups: (((Map[Player, Double], Map[Player, Double]), Set[Group])) => Set[Group] = {
+      case ((hpLoss, mpLoss), groups) =>
+        groups.filter(g =>
+          g.size >= minPlayers &&
+          g.size <= maxPlayers &&
+          g.exists(p => hpLoss(p) >= hpLossThreshold) &&
+          g.exists(p => mpLoss(p) >=  mpLossThreshold))
+    }
+
+    hpLoss.zip(mpLoss)
+          .zip(groups)
+          .sampledBy(Duration.Zero, Duration(1, SECONDS))
+          .map(filterGroups)
+          .filter(_.nonEmpty)
   }
 
   /**
@@ -102,58 +119,39 @@ object Events {
     * 1) not in any lane
     * 2) not in base
     */
-  def roam(game: Int): EventStream[Duration, Set[PlayerState]] = {
-    EventStreamFactory.gameStateStream(game)
-      .filter(gameState => gameState.players.exists(ps => !ps.location.inLane && !ps.location.inBase)) // 1+ roaming
-      .map(gameState => gameState.players.filter(ps => !ps.location.inLane && !ps.location.inBase)
-                                         .toSet) // not in any lane or base
+  def roam(players: Behavior[Duration, Map[Player, PlayerState]]): EventStream[Duration, Group] = {
+    def isRoaming: (Player, PlayerState) => Boolean = {
+      (p, ps) => !ps.location.inLane && !ps.location.inBase
+    }
+
+    def findRoamingPlayers: Map[Player, PlayerState] => Group = {
+      states => states.filter({case (p, ps) => isRoaming(p, ps)})
+                      .toSet
+                      .map({case (p, ps) => ps.player})
+    }
+
+    players.sampledBy(Duration.Zero, Duration(1, SECONDS))
+           .map(findRoamingPlayers)
   }
 
-  private def makeWindows[A](stream: EventStream[Duration, A], window: Duration): List[EventStream[Duration, A]] = {
-    val all = List.empty[EventStream[Duration, A]]
-    stream.getAll.foreach(ei => {
-      if (ei._1 + window <= stream.last._1) {
-        all :+ stream.getAll.filter(en => (en._1 >= ei._1) && (en._1 <= stream.last._1))
-      }
-    })
-    all
-  }
-
-  private def hpLossMet(window: EventStream[Duration, PlayerState], minLossPerSecond: Double): Boolean = {
-    val hpd = window.first._2.championState.hp - window.last._2.championState.hp
-    val td = (window.last._1 - window.first._1).toSeconds
-    (hpd > 0) && (hpd / td) >= minLossPerSecond
-  }
-
-  private def mpLossMet(window: EventStream[Duration, PlayerState], minLossPerSecond: Double): Boolean = {
-    val mpd = window.first._2.championState.mp - window.last._2.championState.mp
-    val td = (window.last._1 - window.first._1).toSeconds
-    (mpd > 0) && (mpd / td) >= minLossPerSecond
-  }
-
-  private def distanceMet(a: LocationData, b: LocationData, max: Double): Boolean = {
-    math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) <= max
+  /**
+    * Players associated with a dragon takedown
+    *
+    * @param takedown Stream of dragon takedown events
+    * @param locations Behavior of all players during a game
+    * @return
+    */
+  def dragon(takedown: EventStream[Duration, Unit],
+             locations: Behavior[Duration, Set[PlayerState]]): EventStream[Duration, Group] = {
+    Behavior.Whenever(locations)
+            .<@(takedown)
+            .map(set => set.filter(ps => ps.location.nearDragon).map(ps => ps.player))
   }
 
   /**
     * A lane swap has occurred if:
-    * 1) first turret has not fallen
+    * 1) Not bot/support vs bot/support in bot lane
     * 2) first minion wave has clashed (approx 1:50)
-    * 3) Not bot/support vs bot/support in bot lane
-    *
-    * @param game The game ID
-    * @return time at which a lane swap occurred
+    * 3) first turret has not fallen
     */
-  private def laneswap(game: Int): EventStream[Duration, Unit] = {
-    EventStreamFactory.gameStateStream(game)
-      .map(gs => {
-        val notInBot = gs.blue.players.count(laneswapRule) + gs.red.players.count(laneswapRule)
-        gs.blue.turrets == 0 && gs.red.turrets == 0 && gs.timestamp.gt(Duration(110, SECONDS)) && notInBot > 0
-      })
-  }
-
-  private def laneswapRule(ps: PlayerState): Boolean = {
-     !ps.location.inBotLane && !ps.location.inBase &&
-       (Role.compare(ps.player.role, Bot()) || Role.compare(ps.player.role, Support()))
-  }
 }
