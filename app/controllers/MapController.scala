2c6748c49c75f0976890295dc01b1a6cef8df84e
Kenneth Lu <krlu91@gmail.com>
2016-08-26 23:17:40 -0400

    synced video parsing typescript with html

--- app/controllers/MapController.scala
+++ app/controllers/MapController.scala
@@ -15,141 +15,141 @@ import play.api.mvc._
 import scala.collection.mutable
 
 class MapController extends Controller {
-	val dbHandler = MongoDBHandler()
+  val dbHandler = MongoDBHandler()
 
-	val levels = List((1, 0),
-										(2, 280),
-										(3,	660),
-										(4,	1140),
-										(5,	1720),
-										(6,	2400),
-										(7,	3180),
-										(8,	4060),
-										(9,	5040),
-										(10, 6120),
-										(11, 7300),
-										(12, 8580),
-										(13, 9960),
-										(14, 11440),
-										(15, 13020),
-										(16, 14700),
-										(17, 16480),
-										(18, 18360))
+  val levels = List((1, 0),
+                    (2, 280),
+                    (3,	660),
+                    (4,	1140),
+                    (5,	1720),
+                    (6,	2400),
+                    (7,	3180),
+                    (8,	4060),
+                    (9,	5040),
+                    (10, 6120),
+                    (11, 7300),
+                    (12, 8580),
+                    (13, 9960),
+                    (14, 11440),
+                    (15, 13020),
+                    (16, 14700),
+                    (17, 16480),
+                    (18, 18360))
 
-	def getName = Action {
-			Ok(views.html.index("hello world"))
-	}
+  def getName = Action {
+    Ok(views.html.index("hello world"))
+  }
 
-	def showRequest = Action { request =>
-		Ok("Got request [" + request + "]")
-	}
+  def showRequest = Action { request =>
+    Ok("Got request [" + request + "]")
+  }
 
-	/*********************************************************************************************************************
-	**************************************** Loading Identifiers for all Games *******************************************
-	*********************************************************************************************************************/
+  /*********************************************************************************************************************
+  **************************************** Loading Identifiers for all Games *******************************************
+  *********************************************************************************************************************/
 
-	def allGames() = Action {
-		val gids: List[Document] = dbHandler.getAllGIDs()
+  def allGames() = Action {
+    val gids: List[Document] = dbHandler.getAllGIDs()
     val arrOfGIDs: mutable.MutableList[JsObject] = new mutable.MutableList[JsObject]
-		for( gid <- gids){
-			val gameKey = gid.getOrElse("gameKey", BsonNull()).asInt32.getValue
-			val vodURL = dbHandler.getYoutubeURLForGame(gameKey)
+    for( gid <- gids){
+      val gameKey = gid.getOrElse("gameKey", BsonNull()).asInt32.getValue
+      val vodURL = dbHandler.getYoutubeURLForGame(gameKey)
       arrOfGIDs.+=:(buildGIDJson(gid, vodURL))
-		}
+    }
     Ok(views.html.Application.games(arrOfGIDs.toList))
-	}
+  }
 
-	def gamesByTeam(teamAcronym: String) = Action {
-		val gids: List[Document] = dbHandler.getGIDsByTeamAcronym(teamAcronym)
-		val arrOfGIDs: mutable.MutableList[JsObject] = new mutable.MutableList[JsObject]
-		for( gid <- gids){
-			val gameKey = gid.getOrElse("gameKey", BsonNull()).asInt32.getValue
-			val vodURL = dbHandler.getYoutubeURLForGame(gameKey)
-			arrOfGIDs.+=:(buildGIDJson(gid, vodURL))
-		}
-		for(element <- arrOfGIDs){
-			element.value.getOrElse("vod", null).toString()
-		}
-		Ok(views.html.Application.games(arrOfGIDs.toList))
-	}
+  def gamesByTeam(teamAcronym: String) = Action {
+    val gids: List[Document] = dbHandler.getGIDsByTeamAcronym(teamAcronym)
+    val arrOfGIDs: mutable.MutableList[JsObject] = new mutable.MutableList[JsObject]
+    for( gid <- gids){
+      val gameKey = gid.getOrElse("gameKey", BsonNull()).asInt32.getValue
+      val vodURL = dbHandler.getYoutubeURLForGame(gameKey)
+      arrOfGIDs.+=:(buildGIDJson(gid, vodURL))
+    }
+    for(element <- arrOfGIDs){
+      element.value.getOrElse("vod", null).toString()
+    }
+    Ok(views.html.Application.games(arrOfGIDs.toList))
+  }
 
-	def buildGIDJson(gameIdentifier : Document, youtubeURL: String): JsObject ={
-		val gameKey: Int = gameIdentifier.getOrElse("gameKey", BsonNull()).asInt32.getValue
-		val gameDate: Long  = gameIdentifier.getOrElse("gameDate", BsonNull()).asInt64.getValue
-		val team1 = gameIdentifier.getOrElse("team1", BsonNull()).asString.getValue
-		val team2 = gameIdentifier.getOrElse("team2", BsonNull()).asString.getValue
-		val realm = gameIdentifier.getOrElse("realm", BsonNull()).asString.getValue
+  def buildGIDJson(gameIdentifier : Document, youtubeURL: String): JsObject ={
+    val gameKey: Int = gameIdentifier.getOrElse("gameKey", BsonNull()).asInt32.getValue
+    val gameDate: Long  = gameIdentifier.getOrElse("gameDate", BsonNull()).asInt64.getValue
+    val team1 = gameIdentifier.getOrElse("team1", BsonNull()).asString.getValue
+    val team2 = gameIdentifier.getOrElse("team2", BsonNull()).asString.getValue
+    val realm = gameIdentifier.getOrElse("realm", BsonNull()).asString.getValue
     val time: Date = new DateTime(gameDate).toDate
-		return Json.obj("gameKey"-> gameKey, "gameDate" -> time.toString,
-			"team1" -> team1, "team2" -> team2, "realm"-> realm, "vodURL"-> youtubeURL.split("v=")(1).split("&t=")(0))
-	}
+    return Json.obj("gameKey"-> gameKey, "gameDate" -> time.toString,
+      "team1" -> team1, "team2" -> team2, "realm"-> realm, "vodURL"-> youtubeURL.split("v=")(1).split("&t=")(0))
+  }
 
-	/*********************************************************************************************************************
-	****************************************** Loading Data From Given Game **********************************************
-	*********************************************************************************************************************/
+  /*********************************************************************************************************************
+  ****************************************** Loading Data From Given Game **********************************************
+  *********************************************************************************************************************/
 
-	def getGameData(gameId: Int, vodID : String) = Action {
+  def getGameData(gameId: Int, vodID : String) = Action {
     println(vodID.replace("\"", ""))
-		val data: List[GameState] = dbHandler.getCompleteGame(gameId)
-		var arrOfStates: JsArray = Json.arr()
-		for(gameState <- data){
-			arrOfStates = arrOfStates.append(buildJson(gameState))
-		}
-    val vodURL = "https://www.youtube.com/embed/" + vodID.replace("\"", "")
+    val data: List[GameState] = dbHandler.getCompleteGame(gameId)
+    var arrOfStates: JsArray = Json.arr()
+    for(gameState <- data){
+      arrOfStates = arrOfStates.append(buildJson(gameState))
+    }
+    val vodURL = vodID.replace("\"", "")
     val json: JsObject = Json.obj("id" -> gameId, "data"-> arrOfStates, "vodURL" -> vodURL)
     Ok(views.html.Application.gamepage(json))
-	}
+  }
 
-	def buildJson(state : GameState): JsObject ={
-		var playerArray = Json.arr()
-		val allPlayers = state.red.players ++ state.blue.players
-		for(player <- allPlayers)
-			playerArray = playerArray.append(buildPlayerStateJson(player))
-		Json.obj("timeStamp" -> state.timestamp.toMillis, "players" -> playerArray)
-	}
+  def buildJson(state : GameState): JsObject ={
+    var playerArray = Json.arr()
+    val allPlayers = state.red.players ++ state.blue.players
+    for(player <- allPlayers)
+      playerArray = playerArray.append(buildPlayerStateJson(player))
+    Json.obj("timeStamp" -> state.timestamp.toMillis, "players" -> playerArray)
+  }
 
-	private def buildPlayerStateJson(playerState: PlayerState): JsObject ={
-		Json.obj("side" -> playerState.sideColor, "player" -> buildPlayerJson(playerState.player),
-			"location" -> buildLocationJson(playerState.location),
-			"champion" -> buildChampionStateJson(playerState.championState))
-	}
-	private def buildPlayerJson(player: Player): JsObject = Json.obj("riotId" -> player.riotId.id,
-		"ign" -> player.ign, "role" -> player.role.name, "team" -> player.team)
+  private def buildPlayerStateJson(playerState: PlayerState): JsObject ={
+    Json.obj("side" -> playerState.sideColor, "player" -> buildPlayerJson(playerState.player),
+      "location" -> buildLocationJson(playerState.location),
+      "champion" -> buildChampionStateJson(playerState.championState))
+  }
+  private def buildPlayerJson(player: Player): JsObject = Json.obj("riotId" -> player.riotId.id,
+    "ign" -> player.ign, "role" -> player.role.name, "team" -> player.team)
 
-	private def buildLocationJson(location: LocationData): JsObject = Json.obj("x" -> location.x,
-		"y" -> location.y, "confidence" -> location.confidence)
+  private def buildLocationJson(location: LocationData): JsObject = Json.obj("x" -> location.x,
+    "y" -> location.y, "confidence" -> location.confidence)
 
-	private def buildChampionStateJson(championState: ChampionState):JsObject = {
-		val lvlData: (Int, Int, Int) = calculateLevel(championState.xp.toInt)
-		Json.obj("hp" -> championState.hp, "mp" -> championState.mp, "cumulativeXP" -> championState.xp,
-			"lvl" -> lvlData._1, "xp" -> lvlData._2, "xpNeededToLvl"-> lvlData._3, "name" -> championState.name)
-	}
+  private def buildChampionStateJson(championState: ChampionState):JsObject = {
+    val lvlData: (Int, Int, Int) = calculateLevel(championState.xp.toInt)
+    Json.obj("hp" -> championState.hp, "mp" -> championState.mp, "cumulativeXP" -> championState.xp,
+      "lvl" -> lvlData._1, "xp" -> lvlData._2, "xpNeededToLvl"-> lvlData._3, "name" -> championState.name)
+  }
 
 
-	/**
-		* Remainder and XP for Next Level default to 0 if current level is 18
-		* @param cumulativeXP
-		* @return (currentLevel, currentXP remainder, XP for Next Level)
-		*/
-	def calculateLevel(cumulativeXP: Int): (Int, Int, Int) = {
-		val filteredLevels: List[(Int, Int)] = levels.filter(tuple => tuple._2 <= cumulativeXP)
-		val levelTuple: (Int, Int) = filteredLevels(filteredLevels.size - 1)
-		val remainder = cumulativeXP - levelTuple._2
-		val xpNeededForNextLvl = xpToObtainLvl(levelTuple._1 + 1) - remainder
-		(levelTuple._1, remainder, xpNeededForNextLvl)
-	}
+  /**
+    * Remainder and XP for Next Level default to 0 if current level is 18
+    * @param cumulativeXP
+    * @return (currentLevel, currentXP remainder, XP for Next Level)
+    */
+  def calculateLevel(cumulativeXP: Int): (Int, Int, Int) = {
+    val filteredLevels: List[(Int, Int)] = levels.filter(tuple => tuple._2 <= cumulativeXP)
+    val levelTuple: (Int, Int) = filteredLevels(filteredLevels.size - 1)
+    val remainder = cumulativeXP - levelTuple._2
+    val xpNeededForNextLvl = xpToObtainLvl(levelTuple._1 + 1) - remainder
+    (levelTuple._1, remainder, xpNeededForNextLvl)
+  }
 
-	/**
-		* @param lvl
-		* @return non-cumulative XP needed to obtain level
-		*/
-	def xpToObtainLvl(lvl : Int): Int = if (lvl < 2 || lvl > 18) 0 else 100 * (lvl - 2) + 280
+  /**
+    * @param lvl
+    * @return non-cumulative XP needed to obtain level
+    */
+  def xpToObtainLvl(lvl : Int): Int = if (lvl < 2 || lvl > 18) 0 else 100 * (lvl - 2) + 280
 }
 
 object MapController {
-	def main(args : Array[String]): Unit ={
-		val mc = new MapController
-		println(mc.calculateLevel(60000))
-	}
+  def main(args : Array[String]): Unit ={
+    val mc = new MapController
+    println(mc.calculateLevel(60000))
+  }
 }
 
--- public/javascripts/src/gg/climb/controllers/VideoController.ts
+++ public/javascripts/src/gg/climb/controllers/VideoController.ts
@@ -2,21 +2,26 @@
 class VideoController {
 };
 
-
-//Load iFrame API code
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 //Create player after API code is done downloading.
 var player;
+var videoId = 'eQ1tJgmNlj4'
+
+function loadVod(newId){
+    videoId = newId
+    console.log(videoId)
+    //Load iFrame API code
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    onYouTubeIframeAPIReady()
+}
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-      height: '200',
-      width: '300',
-      videoId: 'rkqBZx1c11s',
+      height: '400',
+      width: '600',
+      videoId: videoId,
       events: {
         //Trigger this upon creation of player.
       }
@@ -24,14 +29,6 @@ function onYouTubeIframeAPIReady() {
 }
 
 function seek(sec){
-    if(player){
-        player.seekTo(sec, true);
-    }
-}
-
-function jump() {
-
-    var sec = document.getElementById("timeJump").value;
     if(player){
         player.seekTo(sec, true);
     }
