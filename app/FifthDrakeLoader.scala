1d6af4e2ffcffb54b202dc0acda7572d42a3bedf
Prasanth Somasundar <mezuzza@live.com>
2016-10-25 01:52:52 -0700

    Enable compile time dependency injection

---
+++ app/FifthDrakeLoader.scala
@@ -0,0 +1,23 @@
+import controllers.{GameDataController, Health}
+import play.api.ApplicationLoader.Context
+import play.api.routing.Router
+import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
+import router.Routes
+
+/**
+  * Created by prasanth on 10/25/16.
+  */
+class FifthDrakeLoader extends ApplicationLoader {
+  override def load(context: Context): Application =
+    new FifthDrakeApp(context).application
+}
+
+class FifthDrakeApp(context: Context) extends BuiltInComponentsFromContext(context) {
+  lazy val gameDataController = new GameDataController()
+  lazy val healthController = new Health()
+  lazy val assets = new controllers.Assets(httpErrorHandler)
+  lazy val router: Router = new Routes(httpErrorHandler,
+                                       assets,
+                                       gameDataController,
+                                       healthController)
+}
