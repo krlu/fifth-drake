9c2a6118e0940a02ded1403fb30bee640a0b1cf3
Prasanth Somasundar <mezuzza@live.com>
2016-11-04 06:16:52 -0700

    Partial commit

--- app/controllers/GameDataController.scala
+++ app/controllers/GameDataController.scala
@@ -1,8 +1,15 @@
 package controllers
 
+import gg.climb.commons.dbhandling.{DataAccessHandler, MongoDbHandler, PostgresDbHandler}
+import org.mongodb.scala.MongoClient
 import play.api.mvc._
 
 class GameDataController extends Controller {
+
+  val dbh = new DataAccessHandler(
+    new PostgresDbHandler("localhost", 5432, "league_analytics", "", ""),
+    new MongoDbHandler(MongoClient("mongodb://localhost"))
+  )
 
   def xpRequiredForLevel(level: Int): Int =
     if (level > 0 && level <= 18) {
@@ -21,6 +28,12 @@ class GameDataController extends Controller {
       throw new IllegalArgumentException(s"Cannot have `$xp' xp")
     }
 
-  def loadHomePage: Action[AnyContent] = Action {Ok(views.html.index())}
+  def loadHomePage: Action[AnyContent] = Action {
+    Ok(views.html.index())
+  }
+
+  def getTag: Action[AnyContent] = Action {
+    Ok()
+  }
 }
 
