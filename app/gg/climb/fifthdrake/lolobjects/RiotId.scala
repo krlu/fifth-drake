59b178826f20d0cfcee1c838a3bc008ba6a83985
Prasanth Somasundar <mezuzza@live.com>
2016-11-05 14:25:53 -0700

    Use value classes for minor performance boost

--- app/gg/climb/fifthdrake/lolobjects/RiotId.scala
+++ app/gg/climb/fifthdrake/lolobjects/RiotId.scala
@@ -1,14 +1,5 @@
 package gg.climb.fifthdrake.lolobjects
 
-class RiotId[T](val id: String) {
+class RiotId[T](val id: String) extends AnyVal {
   override def toString: String = s"RiotId:$id"
-
-  override def equals(other: Any): Boolean = {
-    other match {
-      case riotId: RiotId[T] => this.id.equals(riotId.id)
-      case _ => false
-    }
-  }
-
-  override def hashCode: Int = id.hashCode
 }
