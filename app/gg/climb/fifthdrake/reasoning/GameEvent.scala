5609016350483ba22137c333c6e03f2f6a42f968
Prasanth Somasundar <mezuzza@live.com>
2016-11-05 09:19:42 -0700

    Fix objective event definition

--- app/gg/climb/fifthdrake/reasoning/GameEvent.scala
+++ app/gg/climb/fifthdrake/reasoning/GameEvent.scala
@@ -6,7 +6,7 @@ import gg.climb.fifthdrake.lolobjects.game.state.LocationData
 
 sealed trait GameEvent
 case class FightEvent(fight: Fight) extends GameEvent
-case class ObjectiveEvent(fight: Fight) extends GameEvent
+case class ObjectiveEvent(objective: Objective) extends GameEvent
 
 sealed class Fight(val playersInvolved : Set[Player], val location : LocationData)
 case class Gank(players : Set[Player], loc : LocationData) extends Fight(players, loc)
