7233d8a6f1f2288b60b71508c630988894d6dcf5
Kenneth Lu <krlu91@gmail.com>
2016-11-08 22:40:01 -0500

    teamJson contains JsonArr of players

--- app/gg/climb/fifthdrake/controllers/GameDataController.scala
+++ app/gg/climb/fifthdrake/controllers/GameDataController.scala
@@ -49,20 +49,28 @@ class GameDataController extends Controller {
   def loadGameData(gameKey: String): Action[AnyContent] = Action { Ok(getGameData(gameKey))}
   def getGameData(gameKey: String): JsObject = {
     def getPlayerStates(igt: InGameTeam, gameLength: Time,
-      samplingRate: Int = 1000): JsObject = {
+      samplingRate: Int = 1000): Seq[JsObject] = {
       val rate = Duration(samplingRate, TimeUnit.MILLISECONDS)
       val start = Duration(0, TimeUnit.MILLISECONDS)
-      igt.playerStates.map { case (p, behavior) =>
+      val playersList = igt.playerStates.map { case (p, behavior) =>
         p -> behavior.sampledBy(start, rate, gameLength)
       }.map { case (p, eventStream) =>
-        p.role.name -> (p.ign, eventStream.getAll.map { case (t, ps) =>
+          val champName = eventStream.getAll.head._2.championState.name
+          val side = eventStream.getAll.head._2.sideColor.name
+          (side, p.role.name, p.ign, champName, eventStream.getAll.map { case (t, ps) =>
           getJsonForPlayerState(p.ign, ps, t)
         })
-      }.foldLeft(Json.obj()) {
-        case (json: JsObject, (roleName, (ign, playerData))) => json ++ Json.obj(
-          roleName -> Json.obj("ign"-> ign, "playerStates" -> playerData)
+      }.toSeq.map{
+        case (side, roleName, ign, champName, playerData) =>
+          Json.obj(
+          "side" -> side,
+          "role" -> roleName,
+          "ign"-> ign,
+          "championName" -> champName,
+          "playerStates" -> playerData
         )
       }
+      playersList
     }
     def getJsonForPlayerState(ign: String,
       playerState: PlayerState,
@@ -73,7 +81,6 @@ class GameDataController extends Controller {
         "y" -> playerState.location.y
       ),
       "championState" -> Json.obj(
-        "championName" -> playerState.championState.name,
         "hp" -> playerState.championState.hp,
         "mp" -> playerState.championState.mp,
         "xp" -> playerState.championState.xp
--- test/gg/climb/fifthdrake/controllers/GameDataControllerTest.scala
+++ test/gg/climb/fifthdrake/controllers/GameDataControllerTest.scala
@@ -1,12 +1,21 @@
 package gg.climb.fifthdrake.controllers
 
 import org.scalatest.{Matchers, WordSpec}
+import play.api.db.Databases
 import play.api.libs.json.{JsArray, JsObject, JsValue}
 
 class GameDataControllerTest extends WordSpec with Matchers{
 
   "A GameDataController" should{
-
+    val database = Databases.inMemory(
+      name = "mydatabase",
+      urlOptions = Map(
+        "MODE" -> "MYSQL"
+      ),
+      config = Map(
+        "logStatements" -> true
+      )
+    )
     val ctrl = new GameDataController
 
     "Return JSON of player states" in {
@@ -15,21 +24,20 @@ class GameDataControllerTest extends WordSpec with Matchers{
       for{
         team: JsValue <- data.values
         teamData = team.as[JsObject]
-        roles <- teamData.values
-        rolesData = roles.as[JsObject]
-        player <- rolesData.values
+        players <- teamData.value.get("playerStats")
+        playersData = players.as[JsArray]
+        player <- playersData.value
         playerData = player.as[JsObject]
-        playerStates = playerData.value("playerStates")
-        playerStatesData = playerStates.as[JsArray]
-        playerStateAtTime <- playerStatesData.value
-        stateAtTimeData = playerStateAtTime.as[JsObject]
+        playerStates <- playerData.value.get("playerStates")
+        stateAtTime <- playerStates.as[JsArray].value
+        stateAtTimeData = stateAtTime.as[JsObject]
         championStateJson = stateAtTimeData.value("championState").as[JsObject]
       }{
         assert(teamData.keys == Set("playerStats"))
-        assert(rolesData.keys == Set("top", "jungle", "mid", "bot", "support"))
-        assert(playerData.keys == Set("ign", "playerStates"))
+        assert(players.as[JsArray].value.size == 5)
+        assert(playerData.keys == Set("side", "role", "ign", "championName", "playerStates"))
         assert(stateAtTimeData.keys == Set("side", "location", "championState"))
-        assert(championStateJson.keys == Set("championName", "hp", "mp", "xp"))
+        assert(championStateJson.keys == Set("hp", "mp", "xp"))
       }
     }
   }
