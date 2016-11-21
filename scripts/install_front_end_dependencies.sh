eae3e0bfb4f105e508ea139ad97d33b837a689a5
Prasanth Somasundar <mezuzza@live.com>
2016-11-20 18:29:07 -0800

    Removed my stupidity

--- scripts/install_front_end_dependencies.sh
+++ scripts/install_front_end_dependencies.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -d ./node_modules ]; then
-	echo "Node modules already installed. Skipping..."
-else
-	npm install
-fi
-
-if [ -d ./elm-stuff ]; then
-	echo "Elm packages already installed. Skipping..."
-else
-	elm package install -y
-fi
+npm install && elm package install -y
