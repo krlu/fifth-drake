69ddb36a7c74409641c90bf8b17a81a7ec298574
Kenneth Lu <krlu91@gmail.com>
2016-12-10 21:11:16 -0500

    put blue and red side in correct order

--- app/gg/climb/fifthdrake/lolobjects/game/state/GameState.scala
+++ app/gg/climb/fifthdrake/lolobjects/game/state/GameState.scala
@@ -6,8 +6,8 @@ import gg.climb.fifthdrake.Time
   * Encapsulates time-slice of in-game data at a timestamp t
   */
 class GameState(val timestamp: Time,
-                val red: (TeamState, Set[PlayerState]),
-                val blue: (TeamState, Set[PlayerState])) {
+                val blue: (TeamState, Set[PlayerState]),
+                val red: (TeamState, Set[PlayerState])) {
 
   override def toString: String = s"GameState(timestamp=$timestamp,\nred=$red,\nblue=$blue)"
 }
