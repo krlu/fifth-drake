bfbe240e694124a7273655e695333d30ae473a79
Prasanth Somasundar <mezuzza@live.com>
2017-01-15 00:16:07 -0800

    Add elm-format precommit hook

--- scripts/add_elm_format_git_hook.sh
+++ scripts/add_elm_format_git_hook.sh
@@ -18,4 +18,12 @@ fi
 # Download the tar file and extract elm-format into the bin folder.
 ELM_FORMAT_DIR="$SCRIPT_DIR/../bin"
 URL="https://github.com/avh4/elm-format/releases/download/0.5.2-alpha/elm-format-0.18-0.5.2-alpha-$OS-x64.tgz"
-curl -L -XGET $URL | tar -xzC $ELM_FORMAT_DIR
+if ! [ -x "$ELM_FORMAT_DIR/elm-format" ]; then
+	echo "Downloading elm-format"
+	curl -L -XGET $URL | tar -xzC $ELM_FORMAT_DIR
+fi
+
+echo "Adding git precommit hook for elm formatting"
+PRECOMMIT_HOOK_SYMLINK_PATH="../../scripts/pre-commit"
+HOOK_DIR="$(git rev-parse --show-toplevel)/.git/hooks"
+ln -s $PRECOMMIT_HOOK_SYMLINK_PATH $HOOK_DIR
