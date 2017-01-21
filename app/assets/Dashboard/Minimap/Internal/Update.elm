b7f80599d394ec12785c8b778c3e784cd1f0499d
evanfrawley <frawley@uw.edu>
2017-01-21 11:58:35 -0800

    Code duplication refactor complete

--- app/assets/Dashboard/Minimap/Internal/Update.elm
+++ app/assets/Dashboard/Minimap/Internal/Update.elm
@@ -8,53 +8,56 @@ import Minimap.Css exposing (CssClass(..), minimapHeight, minimapWidth)
 import Minimap.Types exposing (Action(..), Model, Msg(..), State)
 import Time exposing (second)
 
-percent : Float
-percent = 0.01
-
-minHp : Float
-minHp = 25
+epsilon : Float
+epsilon = 0.00001
 
 onStyle : (Animation.State -> Animation.State) -> State -> State
 onStyle styleFn state =
     { state | style = styleFn state.style }
+
+mapPlayerStates : Data -> Timestamp -> (Side -> Player -> PlayerState -> (Maybe (PlayerId, State))) -> Dict PlayerId State
+mapPlayerStates data timestamp updateIconState =
+  data
+    |> (\{blueTeam, redTeam} ->
+        let
+          teamToIconState : Team -> Side -> List ( PlayerId, State )
+          teamToIconState team side =
+            team.players
+            |> Array.toList
+            |> List.filterMap (\player ->
+              player.state
+              |> Array.get timestamp
+              |> Maybe.andThen (updateIconState side player)
+            )
+        in
+          Dict.fromList <|
+            (teamToIconState blueTeam Blue) ++
+            (teamToIconState redTeam Red)
+        )
 
 update : Model -> Data -> Timestamp -> Msg -> Model
 update model data timestamp msg =
   case msg of
     GenerateIconStates ->
       let
-        newIconState : Dict PlayerId State
-        newIconState =
-          data
-            |> (\{blueTeam, redTeam} ->
-                let
-                  teamToIconState : Team -> Side -> List ( PlayerId, State )
-                  teamToIconState team side =
-                    team.players
-                    |> Array.toList
-                    |> List.filterMap (\player ->
-                      player.state
-                      |> Array.get timestamp
-                      |> Maybe.map (\state ->
-                        ( player.id
-                        , { style =
-                            Animation.style
-                              [ Animation.left (minimapWidth * (state.position.x / model.mapWidth)|> px)
-                              , Animation.bottom (minimapHeight * (state.position.y / model.mapHeight)|> px)
-                              ]
-                          , side = side
-                          , img = player.championImage
-                          }
-                        )
-                      )
-                    )
-                in
-                  Dict.fromList <|
-                    (teamToIconState blueTeam Blue) ++
-                    (teamToIconState redTeam Red)
-                )
+        newIconStates : Dict PlayerId State
+        newIconStates =
+          mapPlayerStates data timestamp
+            (\side player state ->
+              Just <|
+              ( player.id
+              , { style =
+                  Animation.style
+                    [ Animation.left (minimapWidth * (state.position.x / model.mapWidth)|> px)
+                    , Animation.bottom (minimapHeight * (state.position.y / model.mapHeight)|> px)
+                    ]
+                , side = side
+                , img = player.championImage
+                }
+              )
+            )
       in
-        { model | iconStates = newIconState }
+        { model | iconStates = newIconStates }
     AnimatePlayerIcons animationMsg ->
       { model
       | iconStates =
@@ -69,73 +72,59 @@ update model data timestamp msg =
       let
         newIconStates : Dict PlayerId State
         newIconStates =
-          data
-            |> (\{blueTeam, redTeam} ->
-                let
-                  teamToIconState : Team -> Side -> List ( PlayerId, State )
-                  teamToIconState team side =
-                    team.players
-                    |> Array.toList
-                    |> List.filterMap (\player ->
-                      player.state
-                      |> Array.get timestamp
-                      |> Maybe.andThen (\state ->
-                        (Dict.get player.id model.iconStates)
-                        |> Maybe.map (\iconState ->
-                            let
-                              opacity : Float
-                              opacity =
-                                case (((state.championState.hp / state.championState.hpMax) > percent), state.championState.hp > minHp) of
-                                  (False, False) -> 0.0
-                                  (_, _) -> 1.0
-                              newCoordinates : List Property
-                              newCoordinates =
-                                [ Animation.left (minimapWidth * (state.position.x / model.mapWidth)|> px)
-                                , Animation.bottom (minimapHeight * (state.position.y / model.mapHeight)|> px)
-                                , Animation.opacity (opacity)
-                                ]
-                              snap : Animation.State
-                              snap =
-                                ( Animation.interrupt
-                                  [ Animation.set
-                                    newCoordinates
-                                  ]
-                                  iconState.style
-                                )
-                              increment : Animation.State
-                              increment =
-                                ( Animation.queue
-                                  [ Animation.toWith
-                                    ( Animation.easing
-                                      { duration = 1*second
-                                      , ease = (\x -> x)
-                                      }
-                                    )
-                                    newCoordinates
-                                  ]
-                                  iconState.style
-                                )
-                            in
-                              case action of
-                                Snap ->
-                                  ( player.id
-                                  , { iconState
-                                    | style = snap
-                                    }
-                                  )
-                                Increment ->
-                                  ( player.id
-                                  , { iconState
-                                    | style = increment
-                                    }
-                                  )
-                        )
+          mapPlayerStates data timestamp
+            (\side player state ->
+              (Dict.get player.id model.iconStates)
+              |> Maybe.map (\iconState ->
+                  let
+                    opacity : Float
+                    opacity =
+                      case state.championState.hp > epsilon of
+                        False -> 0.0
+                        True -> 1.0
+                    newCoordinates : List Property
+                    newCoordinates =
+                      [ Animation.left (minimapWidth * (state.position.x / model.mapWidth)|> px)
+                      , Animation.bottom (minimapHeight * (state.position.y / model.mapHeight)|> px)
+                      , Animation.opacity (opacity)
+                      ]
+                    snap : Animation.State
+                    snap =
+                      ( Animation.interrupt
+                        [ Animation.set
+                          newCoordinates
+                        ]
+                        iconState.style
                       )
-                    )
-                in
-                  Dict.fromList <|
-                    (teamToIconState blueTeam Blue) ++
-                    (teamToIconState redTeam Red)
-                )
+                    increment : Animation.State
+                    increment =
+                      ( Animation.queue
+                        [ Animation.toWith
+                          ( Animation.easing
+                          -- use timerupdate speed here
+                            { duration = 950*Time.millisecond
+                            , ease = (\x -> x)
+                            }
+                          )
+                          newCoordinates
+                        ]
+                        iconState.style
+                      )
+                  in
+                    case action of
+                      Snap ->
+                        ( player.id
+                        , { iconState
+                          | style = snap
+                          }
+                        )
+                      Increment ->
+                        ( player.id
+                        , { iconState
+                          | style = increment
+                          }
+                        )
+              )
+            )
       in
         { model | iconStates = newIconStates }
