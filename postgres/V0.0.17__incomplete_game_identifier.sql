b8b3f61d31b15de3492a66a2ff506c7f67847c7d
Kenneth Lu <krlu91@gmail.com>
2017-02-04 17:06:05 -0500

    updating incomplete game identifiers

--- postgres/V0.0.17__incomplete_game_identifier.sql
+++ postgres/V0.0.17__incomplete_game_identifier.sql
@@ -2,6 +2,6 @@ CREATE TABLE league.incomplete_game_identifier(
   id SERIAL PRIMARY KEY,
   game_key TEXT NOT NULL,
   game_date TEXT NOT NULL,
-  team_1 TEXT,
-  team_2 TEXT
+  red_team TEXT,
+  blue_team TEXT
 );
