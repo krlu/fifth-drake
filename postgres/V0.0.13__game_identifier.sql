c0462d5947812c610e2b6e1a8b5de7a13e46364d
Kenneth Lu <krlu91@gmail.com>
2017-02-05 22:15:09 -0500

    re-organized game identifier and incomplete identifier

--- postgres/V0.0.13__game_identifier.sql
+++ postgres/V0.0.13__game_identifier.sql
@@ -1,11 +1,14 @@
 CREATE TABLE league.game_identifier(
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   game_key TEXT NOT NULL,
-  blue_team TEXT,
-  red_team TEXT,
-  game_date date NOT NULL,
+  blue_team INTEGER REFERENCES league.team(id),
+  red_team INTEGER REFERENCES league.team(id),
+  game_date TIMESTAMP NOT NULL,
+  game_number INTEGER NOT NULL,
   week Text NOT NULL,
   split TEXT NOT NULL,
+  in_game_hash TEXT NOT NULL,
   series_hash TEXT NOT NULL,
-  mongo collection TEXT NOT NULL
+  realm TEXT NOT NULL,
+  mongo_collection TEXT NOT NULL
 );
--- postgres/V0.0.17__incomplete_game_identifier.sql
+++ postgres/V0.0.17__incomplete_game_identifier.sql
@@ -1,7 +1,11 @@
 CREATE TABLE league.incomplete_game_identifier(
   id SERIAL PRIMARY KEY,
   game_key TEXT NOT NULL,
-  game_date TEXT NOT NULL,
+  blue_team TEXT,
   red_team TEXT,
-  blue_team TEXT
+  game_number INTEGER NOT NULL,
+  in_game_hash TEXT NOT NULL,
+  series_hash TEXT NOT NULL,
+  realm TEXT NOT NULL,
 );
+
