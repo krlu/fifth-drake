c4f4d35ff202dcb0cc0e04f12ee63a0e68d24a2b
Kenneth Lu <krlu91@gmail.com>
2017-02-15 22:37:28 -0500

    fixed parems in tournament.sql

--- postgres/V0.0.13__tournament.sql
+++ postgres/V0.0.13__tournament.sql
@@ -1,7 +1,7 @@
-CREATE TABLE league.tournament{
+CREATE TABLE league.tournament(
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   riot_id TEXT,
   year INTEGER NOT NULL,
   region TEXT NOT NULL,
   sub_season TEXT NOT NULL
-};
+);
