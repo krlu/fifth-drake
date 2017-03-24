6f061d88587a7eb9387d96663862b0cb8452f282
Kenneth Lu <krlu91@gmail.com>
2017-03-23 20:31:43 -0400

    visualizing user groups + refactored settings

--- app/assets/settings.js
+++ app/assets/settings.js
@@ -7,6 +7,7 @@
 	const mountNode = document.getElementById('Content');
 
 	Elm.Settings.embed(mountNode, {
-    searchIcon: require("Settings/ic_search_black_24px.svg")
+    searchIcon: require("Settings/ic_search_black_24px.svg"),
+    addUserIcon: require("Settings/ic_add_white_24px.svg")
 	});
 })();
